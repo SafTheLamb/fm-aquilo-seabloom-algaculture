b579a9774ecf5753d3e05fd2f8c2d91b6531eb3c
sapphiriza <sapphiriza@gmail.com>
2025-02-13 14:29:09 -0800

    Version 0.2.1: Factorissimo compat.

--- base-data-updates.lua
+++ base-data-updates.lua
@@ -18,3 +18,15 @@ ftech.add_unlock("cryogenic-plant", "seabloom-cultivation")
 local aquilo = data.raw.planet["aquilo"]
 aquilo.map_gen_settings.autoplace_controls["seabloom"] = {}
 aquilo.map_gen_settings.autoplace_settings["entity"].settings["seabloom-cluster"] = {}
+
+-- TODO: Use planetlib for this
+for _,entity in pairs(data.raw["agricultural-tower"]) do
+  for _,condition in pairs(entity.surface_conditions or {}) do
+    if condition.property == "pressure" then
+      condition.min = nil
+    end
+    if condition.property == "solar-power" then
+      condition.min = 1
+    end
+  end
+end
